3f361f86d44957c12d38a9abf5f3dae585147373
aurokumarsahu <65843454+aurokumarsahu@users.noreply.github.com>
2021-09-19 16:45:43 +0530

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -2,40 +2,52 @@ var display = document.getElementById("display");
 var s="";
 var btn;
 buttonArray=document.querySelectorAll("button");
-for(x of buttonArray)
-{
-    x.addEventListener("click",(z)=>{
-         btn=z.target.innerText;
-        if(btn=='c')
-        {
-            s="";
-            display.value=s;
-        }
-        else if(btn=='X')
-        {
-           btn='*';
-            s=s+btn;
-            display.value=s;
-        }
-        else if(btn=='%')
-        {
-            btn='*0.01';
-            s=s+btn;
-            display.value=s;
-        }
-        // else if(btn==char(8730))
-        // {
-        //     btntext='Math.Sqrt(';
-        //     s=s+btn;
-        //     display.value=s;
-        // }
-        else if(btn=='=')
-        {
-            display.value=eval(s);
-        }
-        else{
-            s=s+btn;
-            display.value=s;
-        }
-    });
-}
+buttonArray.forEach(x => {
+    x.addEventListener("click",()=>{
+        btn=x.innerText;
+       if(btn=='c')
+       {
+           s="";
+           display.value=s;
+       }
+       else if(btn=='X')
+       {
+          btn='*';
+           s=s+btn;
+           display.value=s;
+       }
+       else if(btn=='%')
+       {
+           btn='*0.01';
+           s=s+btn;
+           display.value=s;
+       }
+       else if(btn=="CE")
+       {
+           s=s.substr(0,s.length-1);
+           display.value=s;
+       }
+       else if(btn=="AC")
+       {
+           s="";
+           display.value=s;
+       }
+
+       // else if(btn==char(8730))
+       // {
+       //     btntext='Math.Sqrt(';
+       //     s=s+btn;
+       //     display.value=s;
+       // }
+       else if(btn=='=')
+       {
+           display.value=eval(s);
+       }
+       else{
+           s=s+btn;
+           display.value=s;
+       }
+   });
+   
+    
+});
